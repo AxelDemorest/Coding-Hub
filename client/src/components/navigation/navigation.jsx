33bb3fb25a070c55701c4c3f3f1ab2a96aeb0fcb
Nohan75 <nohanmarielouise@gmail.com>
2022-09-24 15:52:40 +0200

    start react header

--- client/src/components/navigation/navigation.jsx
+++ client/src/components/navigation/navigation.jsx
@@ -1,24 +1,50 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import TextField from '@mui/material/TextField';
+
+const Nav = styled.nav`
+    display: flex;
+    background: #fffcf6;
+    justify-content: space-between;
+    height: 90px;
+    padding: 0 30px;
+    align-items: center;
+`
+
+const NavBar = styled.div`
+    display: flex;
+    width: 50%;
+`
+const NavItems = styled.ul`
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+`
+
+const NavLink = styled.li`
+    margin: 0 1rem;
+    font-size: 1.2rem;
+    list-style: none;
+    cursor: pointer;
+`
 
 const Navigation = () => {
     return (
-        <div className='navigation'>
+        <Nav className='navigation'>
             <div className="logo">Coding Hub</div>
-            <div className="navbar">
-                <ul>
-                    <li className="navlink">Accueil</li>
-                    <li className="navlink">Tags</li>
-                    <li className="navlink">ddddd</li>
-                </ul>
-                <TextField
+            <TextField
                 id="standard-search"
                 label="Recherche"
                 type="search"
                 variant="standard"
-                />
-            </div>
-        </div>
+            />
+            <NavBar className="navbar">
+                <NavItems className='navItems'>
+                    <NavLink className="navlink">Accueil</NavLink>
+                    <NavLink className="navlink">Tags</NavLink>
+                    <NavLink className="navlink">ddddd</NavLink>
+                </NavItems>
+            </NavBar>
+        </Nav>
     )
 }
 
